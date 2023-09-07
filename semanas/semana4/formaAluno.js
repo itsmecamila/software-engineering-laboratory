8cb0f67e2fef123b9d1809d46a48558b58d04f7e
itsmecamila <camilamarq.depaula@gmail.com>
2023-09-06 22:53:56 -0300

    exercicio 1 - completo

--- semanas/semana4/formaAluno.js
+++ semanas/semana4/formaAluno.js
@@ -1,4 +1,3 @@
-
 class Pessoa{
     constructor(nome,email,telfixo,celular,datanascimento){
         this.nome = nome;
@@ -23,58 +22,14 @@ class Aluno extends Pessoa {
       
 }
 
-class Professor extends Pessoa {
-    constructor(nome,email,telfixo,celular,datanascimento,area,matricula,lattes){
-        super(nome,email,telfixo,celular,datanascimento);
-        this.area = area;
-        this.matricula = matricula;
-        this.lattes = lattes;
-    }
-
-    apresentar() {
-        const informacoes = `Nome: ${this.nome}, Email: ${this.email}, Telefone Fixo: ${this.telfixo}, Celular: ${this.celular}, Data de Nascimento: ${this.datanascimento}, Área: ${this.area}, Matrícula: ${this.matricula}, Lattes: ${this.lattes}`;
-        return informacoes;
-      }
-      
-}
-
-const formularioProfessor = document.getElementById("cadastroProfessor");
 const formularioAluno = document.getElementById("cadastroAluno");
 const resultadoCA = document.getElementById("resultadoCA");
-const resultadoCP = document.getElementById("resultadoCP");
 const cadastrosAluno = [];
-const cadastrosProfessor = [];
-
-
-formularioProfessor.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const formData = new FormData(formularioProfessor);
-
-  const nome = formData.get("nome");
-  const email = formData.get("email");
-  const telfixo = formData.get("telfixo");
-  const celular = formData.get("celular");
-  const datanascimento = formData.get("datanasc");
-  const area = formData.get("area");
-  const matricula = formData.get("matricula");
-  const lattes = formData.get("lattes");
-  const professor = new Professor(nome,email,telfixo,celular,datanascimento,area,matricula,lattes);
-  cadastrosProfessor.push(professor);
-
-
-  resultadoCP.innerHTML = "";
-  cadastrosProfessor.forEach((cadastro) => {
-        const p = document.createElement("p");
-        p.textContent = cadastro.apresentar();
-        resultadoCP.appendChild(p);
-});
-});
 
 
 formularioAluno.addEventListener("submit", function (event) {
     event.preventDefault();
-  
+    console.log('entrei');
     const formData = new FormData(formularioAluno);
   
     const nome = formData.get("nome");
@@ -86,6 +41,7 @@ formularioAluno.addEventListener("submit", function (event) {
     const datanascimento = formData.get("datanasc");
 
     const aluno = new Aluno(nome,email,telfixo,celular,datanascimento,curso,matricula);
+    console.log(aluno);
     cadastrosAluno.push(aluno);
 
     resultadoCA.innerHTML = "";
